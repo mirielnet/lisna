ca71c1ef0f75fcfb59a6c0c719d5c47434f94224
Miriel <me@miriel.net>
2024-08-25 14:34:08 +0900

    Feat: 一部を Basic認証で保護

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import asyncio
 import logging
 import os
+import secrets
 from contextlib import asynccontextmanager
 
 import aiofiles
@@ -11,17 +12,20 @@ import discord
 import core.connect
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Depends, HTTPException, status, Request
+from fastapi.responses: HTMLResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.staticfiles import StaticFiles
 from jinja2 import Template
 
 from version import BOT_VERSION
 from core.bot import MWBot
 
-# .envファイルからトークンを読み込み
+# .envファイルからトークンと管理者のユーザー名とパスワードを読み込み
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
 # ロギングの設定
 logging.basicConfig(
@@ -36,15 +40,25 @@ intents.message_content = True
 # ボットのインスタンスを作成
 bot = MWBot(command_prefix="!", intents=intents)
 
+# Basic認証の設定
+security = HTTPBasic()
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, ADMIN_USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, ADMIN_PASSWORD)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     asyncio.create_task(start_bot())
     yield
 
-
 app = FastAPI(lifespan=lifespan)
-
 
 # グローバルスラッシュコマンドの登録
 @bot.event
@@ -54,12 +68,10 @@ async def on_ready():
 
     logger.info(f"{bot.user}がDiscordに接続されました。")
 
-
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandError):
         logger.error(f"Error in command {ctx.command}: {error}")
-
 
 @tasks.loop(minutes=5)  # 5分ごとに実行
 async def update_status():
@@ -69,13 +81,17 @@ async def update_status():
     await bot.change_presence(status=discord.Status.online, activity=activity)
     logger.info(f"ステータスが更新されました: {server_count}サーバー")
 
-
 # 静的ファイルの設定
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
-# index.htmlを表示するエンドポイント
+# /static/index.htmlを表示するエンドポイント
 @app.get("/", response_class=HTMLResponse)
+async def read_root():
+    async with aiofiles.open("static/index.html", mode="r", encoding="utf-8") as f:
+        return HTMLResponse(await f.read())
+
+# /admin/index.htmlを表示するエンドポイント
+@app.get("/admin/", response_class=HTMLResponse, dependencies=[Depends(authenticate)])
 async def read_index(request: Request):
     guilds_info = []
     for guild in bot.guilds:
@@ -91,11 +107,10 @@ async def read_index(request: Request):
             }
         )
 
-    async with aiofiles.open("static/index.html", mode="r", encoding="utf-8") as f:
+    async with aiofiles.open("static/admin/index.html", mode="r", encoding="utf-8") as f:
         template = Template(await f.read())
     content = template.render(server_count=len(bot.guilds), guilds=guilds_info)
     return HTMLResponse(content=content)
-
 
 async def get_existing_invite(guild):
     for channel in guild.text_channels:
@@ -108,7 +123,6 @@ async def get_existing_invite(guild):
             continue
     return await create_invite(guild)
 
-
 async def create_invite(guild):
     for channel in guild.text_channels:
         try:
@@ -118,11 +132,9 @@ async def create_invite(guild):
             continue
     return "招待リンクを作成できませんでした。"
 
-
 # ボットの起動
 async def start_bot():
     await bot.start(TOKEN)
-
 
 if __name__ == "__main__":
     import uvicorn
