3b13c8654cc6bb0964e9bc24633981826e3c6496
Miriel <me@miriel.net>
2024-08-27 15:37:30 +0900

    Fix: Wikipedia

--- cogs/wikipedia.py
+++ cogs/wikipedia.py
@@ -16,7 +16,8 @@ class Wikipedia(commands.Cog):
         await interaction.response.defer()
 
         try:
-            async with httpx.AsyncClient() as client:
+            async with httpx.AsyncClient(follow_redirects=True) as client:
+                # Get the initial response with the possibility of a 302 redirect
                 response = await client.get(f"https://ja.wikipedia.org/api/rest_v1/page/summary/{httpx.URL(word)}")
                 data = response.json()
 
@@ -30,11 +31,11 @@ class Wikipedia(commands.Cog):
 
             await interaction.followup.send(embed=embed)
 
-        except Exception:
+        except Exception as e:
             embed = discord.Embed(
                 color=discord.Color.red(),
                 title="取得できませんでした",
-                description="検索ワードを変えて、もう一度実行してください。"
+                description=f"検索ワードを変えて、もう一度実行してください。\nエラー: {str(e)}"
             )
             await interaction.followup.send(embed=embed, ephemeral=True)
 
