3534736059fa9b7f38bfb854e881f2b4497e3722
Miriel <me@miriel.net>
2024-08-29 09:47:39 +0900

    Fix

--- cogs/bump.py
+++ cogs/bump.py
@@ -2,7 +2,7 @@
 # Author: Miriel (@mirielnet)
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 import asyncio
 
 class BumpNotify(commands.Cog):
@@ -11,15 +11,26 @@ class BumpNotify(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        # DISBOARDのBUMP通知処理
+        # Embedが存在するか確認
+        if not message.embeds:
+            print("Embedが存在しません。")
+            return
+        
+        embed = message.embeds[0]
+        
+        # DISBOARDのBUMP通知処理 (2時間後)
         if message.author.id == 302050872383242240:  # DISBOARDのBOT ID
-            embed_description = message.embeds[0].description if message.embeds else ""
+            embed_description = embed.description if embed.description else ""
+            print(f"DISBOARD Embed Description: {embed_description}")
+
             if "表示順をアップしたよ" in embed_description or "Bump done" in embed_description:
                 await self.send_bump_notification(message.channel, 7200, "BUMPの時間です\n</bump:947088344167366698>でサーバーの表示順位を上げよう！")
 
-        # DIS速のBUMP通知処理
+        # DIS速のBUMP通知処理 (1時間後)
         elif message.author.id == 981314695543783484:  # DIS速のBOT ID
-            embed_author_name = message.embeds[0].author.name if message.embeds and message.embeds[0].author else ""
+            embed_author_name = embed.author.name if embed.author and embed.author.name else ""
+            print(f"DIS速 Embed Author Name: {embed_author_name}")
+
             if "アップしたよ!" in embed_author_name:
                 await self.send_bump_notification(message.channel, 3600, "UPの時間です\n</up:1135405664852783157>でサーバーの表示順位を上げよう！")
 
@@ -39,7 +50,7 @@ class BumpNotify(commands.Cog):
                 color=discord.Color.green() if "UPの時間" in reminder_text else discord.Color.white()
             ))
         except discord.Forbidden:
-            print(f"Cannot send messages in {channel.name}")
+            print(f"Cannot send messages in {channel.name} due to lack of permissions")
 
 async def setup(bot):
     await bot.add_cog(BumpNotify(bot))
