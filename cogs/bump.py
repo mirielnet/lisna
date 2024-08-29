7733622311aa3af6a37699d58b3ca58166d187da
Miriel <me@miriel.net>
2024-08-29 10:50:31 +0900

    Fix?

--- cogs/bump.py
+++ cogs/bump.py
@@ -10,17 +10,26 @@ class BumpNotify(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_interaction(self, interaction: discord.Interaction):
-        if interaction.type == discord.InteractionType.application_command:
-            # DIS速のコマンドが実行された場合
-            if interaction.user.id == 761562078095867916 and "up" in interaction.command.name:
-                await self.send_initial_notification(interaction.channel, "UP通知", "UPを受信しました\n1時間後に通知します")
-                await self.send_bump_notification(interaction.channel, 3600, "UPの時間です\n</up:1135405664852783157>でサーバーの表示順位を上げよう！")
+    async def on_message(self, message: discord.Message):
+        # メッセージの送信者がDIS速のボットであることを確認
+        if message.author.id == 761562078095867916:  # ここにDIS速のボットIDを入れてください
+            if message.embeds:
+                embed = message.embeds[0]  # 最初のEmbedを取得
                 
-            # DISBOARDの /bump コマンド処理
-            elif interaction.user.id == 302050872383242240 and interaction.command.name == "bump":
-                await self.send_initial_notification(interaction.channel, "BUMP通知", "BUMPを受信しました\n2時間後に通知します")
-                await self.send_bump_notification(interaction.channel, 7200, "BUMPの時間です\n</bump:947088344167366698>でサーバーの表示順位を上げよう！")
+                # Embedのcommand部分に「/dissoku up」が含まれているかを確認
+                if "command: /dissoku up" in embed.description:
+                    await self.send_initial_notification(message.channel, "UP通知", "UPを受信しました\n1時間後に通知します")
+                    await self.send_bump_notification(message.channel, 3600, "UPの時間です\n</up:1135405664852783157>でサーバーの表示順位を上げよう！")
+
+        # DISBOARDのボットIDでメッセージを検知
+        elif message.author.id == 302050872383242240:  # DISBOARDのボットID
+            if message.embeds:
+                embed = message.embeds[0]  # 最初のEmbedを取得
+
+                # Embedのdescriptionに特定の内容が含まれているかを確認
+                if "表示順をアップしたよ" in embed.description:
+                    await self.send_initial_notification(message.channel, "BUMP通知", "BUMPを受信しました\n2時間後に通知します")
+                    await self.send_bump_notification(message.channel, 7200, "BUMPの時間です\n</bump:947088344167366698>でサーバーの表示順位を上げよう！")
 
     async def send_initial_notification(self, channel, title, description):
         try:
